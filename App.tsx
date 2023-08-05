fa86c1762b7eafc385f7ac018bd30f16971057fc
kimba <skyqwn@gmail.com>
2023-08-05 16:10:27 +0900

    init

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import tw from "twrnc";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={tw`text-red-500 font-bold`}>Hello Dev!!</Text>
+      <Text style={tw`text `}></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
